4eca7202730bfd5aeb6ff617438e0f2d2df9a3f9
Minjae Lee <ipjaworld@gmail.com>
2022-11-23 13:01:30 +0900

    am fixed and add Function

--- MySqlDataBase/src/01_DDL.sql
+++ MySqlDataBase/src/01_DDL.sql
@@ -37,3 +37,80 @@ SELECT * FROM scott.booklist;
 
 
 
+-- 연습 문제.
+-- 아래의 필드명을 담은 memberlist 테이블을 생성하세요
+-- 필드명 : membernum(int, 자동 증가, 기본키, not null), name(varchar(30), not null),
+--			 Birth(date, not null), bpoint(int), joindate(date), age(int), gender(varchar(3)
+-- 테이블  comment : 회원 목록
+-- 기본 문자 set : utf-8mb4
+-- 워크 벤치에서 테이블 생성하는 명령을 복사하는 것 권장
+
+
+CREATE TABLE `scott`.`memberlist` (
+  `membernum` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30) NOT NULL,
+  `Birth` DATE NOT NULL,
+  `bpoint` INT NULL,
+  `joindate` DATE NULL,
+  `age` INT NULL,
+  `gender` VARCHAR(3) NULL,
+  PRIMARY KEY (`membernum`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COMMENT = '회원 목록';
+
+SELECT * FROM SCOTT.MEMBERLIST;
+
+-- 테이블의 수정
+ALTER TABLE `scott`.`memberlist` 
+CHANGE COLUMN `Birth` `Birth` DATETIME NULL DEFAULT NULL ,
+CHANGE COLUMN `joindate` `joindate` DATETIME NULL DEFAULT NOW();
+
+
+-- 연습문제2
+-- 테이블 이름 : rentlist
+-- 필드 : rentdate(datetime, default now()),	numseq(int, AI), booknum( int )
+--		   membernum(int), discount( int )
+
+CREATE TABLE `scott`.`rentlist` (
+  `rentdate` DATETIME NOT NULL DEFAULT now(),
+  `numseq` INT NOT NULL AUTO_INCREMENT,
+  `booknum` INT NOT NULL,
+  `membernum` INT NOT NULL,
+  `discount` INT NULL DEFAULT 0,
+  PRIMARY KEY (`numseq`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COMMENT = '책 빌려간 내역';
+
+select * from scott.rentlist;
+
+
+
+
+
+
+-- 외래키1 : rentlist booknum 은 booklist 의 booknum을 참조
+-- 외래키2 : rentlist membernum 은 memberlist 의 membernum 을 참조
+
+
+ALTER TABLE `scott`.`rentlist` 
+ADD INDEX `fk1_idx` (`booknum` ASC) VISIBLE,
+ADD INDEX `fk2_idx` (`membernum` ASC) VISIBLE;
+;
+ALTER TABLE `scott`.`rentlist` 
+ADD CONSTRAINT `fk1`
+  FOREIGN KEY (`booknum`)
+  REFERENCES `scott`.`booklist` (`booknum`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `fk2`
+  FOREIGN KEY (`membernum`)
+  REFERENCES `scott`.`memberlist` (`membernum`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+
+
+
+
